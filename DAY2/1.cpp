1502933d24a675204b717d87176c63509b429039
Tarun Kumar <tarun.kumar@bigohtech.com>
2022-01-20 08:47:48 +0530

    DAY2_2

--- DAY2/1.cpp
+++ DAY2/1.cpp
@@ -23,21 +23,23 @@ void bubble_sort(int a[],int n)
 
 int removeDuplicates(int arr[], int n)
 {
+    //sorting the array in order to have all duplicates in row
     bubble_sort(arr,n);
+    //checking if array is empty
     if (n==0 || n==1)
         return n;
 
     int temp[n];
     int j = 0;
     for (int i=0; i<n-1; i++)
-
+    //if element not same as previous then storing it
         if (arr[i] != arr[i+1])
             temp[j++] = arr[i];
 
-
+    //storing last element
     temp[j++] = arr[n-1];
 
-
+    //placing elements in orignal array
     for (int i=0; i<j; i++)
         arr[i] = temp[i];
 
@@ -46,12 +48,14 @@ int removeDuplicates(int arr[], int n)
 
 int main()
 {
+    //array input
     int arr[] = {10, 12, 12, 3, 1, 1, 4, 5, 5};
+
     int n = sizeof(arr) / sizeof(arr[0]);
 
-    n = removeDuplicates(arr, n);
+    newSize = removeDuplicates(arr, n);
 
-    for (int i=0; i<n; i++)
+    for (int i=0; i<newSize; i++)
        cout << arr[i] << " ";
 
     return 0;

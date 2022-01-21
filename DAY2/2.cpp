dd11ad4e105f9b7dd975e271a1c599a29a0168f1
Tarun Kumar <tarun.kumar@bigohtech.com>
2022-01-21 07:34:58 +0530

    DAY2_2

--- DAY2/2.cpp
+++ DAY2/2.cpp
@@ -10,9 +10,23 @@ using namespace std;
   be ordered in ascending order with respect to the numbers they hold. If no three numbers sum up to the
    target sum, the function should return an empty array
 */
+void bubble_sort(int a[],int n)
+{
+
+    for (int i=0;i<n-1;i++)
+    {
+        for (int j=0;j<n-1-i;j++)
+        {
+            if(a[j]>a[j+1])
+                swap(a[j],a[j+1]);
+        }
+    }
+}
+
+
 vector<vector<int>> findTriplets(int arr[], int n, int target)
 {
-   sort(arr,arr+n);
+    bubble_sort(arr,n);
 
     vector<vector<int>> ans;
     bool found = false;
@@ -22,29 +36,37 @@ vector<vector<int>> findTriplets(int arr[], int n, int target)
         {
             for (int k=j+1; k<n; k++)
             {
-
-                if (arr[i]+arr[j]+arr[k] == target){
-                     ans.push_back({arr[i],arr[j],arr[k]}); 
+                if (arr[i]+arr[j]+arr[k] == target)
+                {
+                
+                    ans.push_back({arr[i],arr[j],arr[k]}); 
+                    found = true;
                 }
             }
         }
     }
 
-  sort(ans.begin(),ans.end());
+    if (found == false)
+        cout << " not exist "<<endl;
+
     return ans;
 }
 
 int main()
 {
-    int arr[] = {1,2,3,4},target=6;
-    int n=10;
-    vector<vector<int>>ans=findTriplets(arr,n,target);
-     
-    for(auto x:ans)
-        cout<<x[0]<<" "<<x[1]<<" "<<x[2]<<endl;
+    int arr[] = {0,1,2,6,4,3,5},target;
+    int noElements = sizeof(arr)/sizeof(arr[0]);
+    cin>>target;
+    vector<vector<int>> v=findTriplets(arr,noElements,target);
     
-     
-
-
+    for (int i = 0; i < v.size(); i++)
+    {
+        for (int j = 0; j < v[i].size(); j++)
+        {
+            cout << v[i][j] << " ";
+        }   
+        cout << endl;
+    }
+           
     return 0;
 }

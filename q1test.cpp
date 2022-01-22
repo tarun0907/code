01e36393dce148d21399e8e4b8731aac2aa241b6
Tarun Kumar <tarun.kumar@bigohtech.com>
2022-01-22 13:38:58 +0530

    question 1

--- q1test.cpp
+++ q1test.cpp
@@ -1,18 +1,6 @@
 #include<iostream>
 using namespace std;
 
-void bubbleSort(int a[],int n)
-{
-
-    for (int i=0;i<n-1;i++)
-    {
-        for (int j=0;j<n-1-i;j++)
-        {
-            if(a[j]>a[j+1])
-                swap(a[j],a[j+1]);
-        }
-    }
-}
 
 int medianOfArrays(int arr1[],int arr2[],int size1,int size2)
 {
@@ -33,7 +21,7 @@ int medianOfArrays(int arr1[],int arr2[],int size1,int size2)
 
     
 
-    bubbleSort(arr3,size1+size2);
+ 
     
     int median=(size1+size2)/2;
     return arr3[median];
@@ -43,7 +31,7 @@ int medianOfArrays(int arr1[],int arr2[],int size1,int size2)
 int main()
 {
     
-    int arr1[]={4,5,1},arr2[]={9,6,8,10,11,7};
+    int arr1[]={1,2,3},arr2[]={4,5,6,7,8,9};
     int size1=sizeof(arr1) / sizeof(arr1[0]);
     int size2=sizeof(arr2)/sizeof(arr2[0]);
     cout <<medianOfArrays(arr1,arr2,size1,size2)<<endl;
